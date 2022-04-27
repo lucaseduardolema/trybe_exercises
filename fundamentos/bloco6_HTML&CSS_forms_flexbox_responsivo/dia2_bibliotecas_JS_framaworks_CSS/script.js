b3f6de8f1232e59b202e45ff46e863f618428ea1
lucaseduardolema <lucaseduardolema@gmail.com>
2022-04-27 20:29:10 -0300

    add exercise

---
+++ fundamentos/bloco6_HTML&CSS_forms_flexbox_responsivo/dia2_bibliotecas_JS_framaworks_CSS/script.js
@@ -0,0 +1,6 @@
+// 1 - Adicione um framework CSS de sua escolha ao formulário que você construiu na última aula e o utilize para estilizar o formulário.
+
+function submit(event) {
+  event.preventDefault();
+}
+// document.getElementById('submit').addEventListener('click', submit);
