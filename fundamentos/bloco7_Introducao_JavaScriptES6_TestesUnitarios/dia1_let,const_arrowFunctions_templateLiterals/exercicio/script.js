873d0a268def50ac3f5b1dd6aa55bff535995437
lucaseduardolema <lucaseduardolema@gmail.com>
2022-05-09 11:36:44 -0300

    add exercises

---
+++ fundamentos/bloco7_Introducao_JavaScriptES6_TestesUnitarios/dia1_let,const_arrowFunctions_templateLiterals/exercicio/script.js
@@ -0,0 +1,49 @@
+// Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
+// Modifique a estrutura da função para que ela seja uma arrow function.
+// Modifique as concatenações para template literals.
+
+function testingScope(escopo) {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    console.log(ifScope);
+  } else {
+    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+}
+
+testingScope(true);
+
+// 🚀 Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
+// Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>); retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+// Bônus (opcional): tente fazer o mesmo exercício utilizando o métodoarray.sort(). Spoiler: É possível realizar uma função que ordene qualquer array de números.
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+oddsAndEvens.sort((a, b) => a - b)
+
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
+
+// Crie uma função que receba um número e retorne seu fatorial.
+// Na matemática, o fatorial de um número não negativo N, com a notação N!, é o produto de todos os inteiros menores ou iguais a N. Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
+// Bônus (opcional): tente fazer o mesmo exercício de forma recursiva. Spoiler: É possível resolver com uma linha usando ternary operator.
+
+// const fatorial = (num) => {
+//   num === 0 ? num : 
+// }
+
+// Crie uma função que receba uma frase e retorne qual a maior palavra.
+
+const longestWord = (str) => {
+  let arr = str.split(' ');
+  let result;
+  for (let value of arr) {
+    result = arr[0].length > value.length ? arr[0] : value
+  }
+  return result
+}
+
+console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu')) // retorna 'aconteceu'
+
