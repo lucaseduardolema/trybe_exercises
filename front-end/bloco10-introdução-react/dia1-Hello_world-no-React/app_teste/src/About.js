130a872e63713a78e62788562205c84cfe466c7d
lucaseduardolema <lucaseduardolema@gmail.com>
2022-06-20 18:49:43 -0300

    add exercises

---
+++ front-end/bloco10-introdução-react/dia1-Hello_world-no-React/app_teste/src/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Lucas Eduardo</h1>
+        <p>teste do primeiro componente criado</p>
+      </div>
+    )
+  }
+}
+
+export default About
